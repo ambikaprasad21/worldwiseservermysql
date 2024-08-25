b4bd04b4583d71c9acf4b37400d6c54403577857
Ambika Prasad <akashboard2021@gmail.com>
2024-08-26 00:47:06 +0530

    added db to env

--- utils/db.js
+++ utils/db.js
@@ -1,10 +1,10 @@
 import mysql from "mysql2";
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "mysql2024@ambika",
-  database: "worldwise",
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.DATABASE,
 });
 
 export default pool;
