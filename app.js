3a676ed70c21c68466730dcd89d3eeff439a47af
Ambika Prasad <akashboard2021@gmail.com>
2024-08-26 01:46:00 +0530

    update cors

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const app = express();
 
 app.use(
   cors({
-    origin: "*",
+    origin: [
+      "http://localhost:5173",
+      "https://worldwiseservermysql.onrender.com",
+    ],
     methods: ["GET", "POST", "DELETE"],
   })
 );
